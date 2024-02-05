ede624157046a578deaa0a1c2dc95b437a378972
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-05 23:36:10 +0530

    updated Main.java file

--- linkedLists/singlyLinkedList/Main.java
+++ linkedLists/singlyLinkedList/Main.java
@@ -6,9 +6,9 @@ public class Main {
         list.insertFirst(45);
         list.insertFirst(34);
         list.insertFirst(67);
-        // list.display();
-        // list.insertLast(90);
-        // list.display();
+        list.display();
+        list.insertLast(90);
+        list.display();
         list.insertWithoutTail(87);
         list.display();
         list.insert(35, 2);
