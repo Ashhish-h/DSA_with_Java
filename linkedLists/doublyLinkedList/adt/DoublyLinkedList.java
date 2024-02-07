88accf8aecf3b3edd7f4aa852bdded881683ea94
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-08 00:24:18 +0530

    added abstract fun's in DoublyLinkedList interface

--- linkedLists/doublyLinkedList/adt/DoublyLinkedList.java
+++ linkedLists/doublyLinkedList/adt/DoublyLinkedList.java
@@ -1,5 +1,28 @@
 package linkedLists.doublyLinkedList.adt;
 
+import linkedLists.doublyLinkedList.node.Node;
+
 public interface DoublyLinkedList {
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public void addFirst(int val);
+
+    public void addLast(int val);
+
+    public void add(int val, int index);
+
+    public int removeFirst();
+
+    public int removeLast();
+
+    public int remove(int index);
+
+    public int search(int val);
+
+    public Node search();
+
     
 }
