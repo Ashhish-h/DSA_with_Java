00ec652bbe931022a80e1bb4f3e664a8d4c7fded
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-08 00:12:51 +0530

    created Node class and added toString() method

--- linkedLists/doublyLinkedList/node/Node.java
+++ linkedLists/doublyLinkedList/node/Node.java
@@ -1,5 +1,28 @@
 package linkedLists.doublyLinkedList.node;
 
 public class Node {
+
+    private int value;
+    private Node next;
+    private Node previous; 
+
+    public Node(int value){
+        this.value = value;
+    }
+
+    public Node(int value, Node next, Node previous){
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
     
+    @Override
+	public String toString() {
+		return "{"
+				+ (previous == null ? "null" : previous.value)
+				+ " <--> " + this.value + " <--> "
+				+ (next == null ? "null" : next.value)
+				+ "}";
+    }
+
 }
