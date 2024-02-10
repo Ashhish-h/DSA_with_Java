99b36b9985a423457f49c6528d185e2cd6449dc0
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-11 00:15:04 +0530

    implemented list.addFirst() and print() method

--- linkedLists/doublyLinkedList/main/Main.java
+++ linkedLists/doublyLinkedList/main/Main.java
@@ -1,5 +1,13 @@
 package linkedLists.doublyLinkedList.main;
 
+import linkedLists.doublyLinkedList.DoublyLinkedList.IntegerDoublyLinkedList;
+
 public class Main {
-    
+    public static void main(String[] args){
+        IntegerDoublyLinkedList list = new IntegerDoublyLinkedList();
+        list.addFirst(90);
+        list.addFirst(67);
+        list.addFirst(89);
+        list.print();
+    }
 }
