7e93bbe032c3c4ed48c1ccf81cd1aeb7ed715b04
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-11 14:26:53 +0530

    implement methods of CircularLinkedList

--- linkedLists/circularLinkedList/Main.java
+++ linkedLists/circularLinkedList/Main.java
@@ -2,6 +2,10 @@ package linkedLists.circularLinkedList;
 
 public class Main {
     public static void main(String[] args){
-        
+        CircularLinkedList list = new CircularLinkedList();
+        list.add(89);
+        list.add(70);
+        list.add(0);
+        list.print();
     }
 }
