3fde24d5d17c446b5f024ec8088bc24d9d674d60
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-11 13:46:24 +0530

    import related packages and added makeCircular() method to make a linkedlist circular

--- linkedLists/circularLinkedList/CircularLinkedList.java
+++ linkedLists/circularLinkedList/CircularLinkedList.java
@@ -1,5 +1,13 @@
 package linkedLists.circularLinkedList;
 
-public class CircularLinkedList {
-    
+import linkedLists.doublyLinkedList.DoublyLinkedList.IntegerDoublyLinkedList;
+import linkedLists.doublyLinkedList.node.Node;
+
+public class CircularLinkedList extends IntegerDoublyLinkedList {
+    public void makeCircular() {
+		if (head != null) {
+			tail.next = head;
+			head.previous = tail;
+		}
+	}
 }
