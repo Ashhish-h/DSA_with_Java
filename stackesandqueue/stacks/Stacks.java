f9d210133d64a671a74084932a9c355aa416c62c
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-12 23:10:22 +0530

    create Stack class to implement stack DS create inner class Node defining properties of Node add isUnderFlow() and getSize() method in Stack class to check if stack is empty or not and get to size of the stack

---
+++ stackesandqueue/stacks/Stacks.java
@@ -0,0 +1,43 @@
+package stackesandqueue.stacks;
+
+public class Stacks {
+
+    int size; //size of the stack
+
+    Node top; //Node pointing to the top element of the stack
+
+    // Method to check is stack is empty or not
+    boolean isUnderflow(){
+        return size == 0;
+    }
+
+    // method to get the size of the stack
+    public int getSize(){
+        return size;
+    }
+
+
+
+
+
+
+
+    //Node class defination for stack
+    static class Node{
+        int value;
+
+        Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
