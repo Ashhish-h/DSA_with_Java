a4e54bf5fd283d529e9552ed24b22282f9e440ed
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-12 23:32:12 +0530

    create stack object and add nodes to stack

--- stacks/stacks/Main.java
+++ stacks/stacks/Main.java
@@ -2,6 +2,10 @@ package stacks.stacks;
 
 public class Main {
     public static void main(String[] args){
-
+        Stack stack = new Stack();
+        stack.push(90);
+        stack.push(85);
+        stack.push(78);
+        stack.push(99);
     }
 }
