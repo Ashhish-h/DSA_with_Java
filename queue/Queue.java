1dee5f097b3ca9ba459a9a323841953fd5b486e5
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-20 23:08:26 +0530

    create QueueNode inner class to implement node of queue. add front and rear pointer in Queue class

--- queue/Queue.java
+++ queue/Queue.java
@@ -1,5 +1,19 @@
 package queue;
 
 public class Queue {
+    public QueueNode front;
+    public QueueNode rear;
+    public int size;
+
     
+
+    public static class QueueNode{
+        int value;
+        QueueNode next;
+        public QueueNode(int value){
+            this.value = value;
+            this.next = null;
+        }
+
+    }
 }
