5d516ac95f4cd8d82ddfc66bc87dc8f9a62e0285
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-21 19:34:24 +0530

    add method isEmpty() and and getSize() method

--- queue/Queue.java
+++ queue/Queue.java
@@ -5,8 +5,18 @@ public class Queue {
     public QueueNode rear;
     public int size;
 
+    //isEmpty() method to check if queue is empty or not
+    boolean isEmpty(){
+        return size == 0;
+    }
+
+    //getSize() method to get the size of the queue
+    int getSize(){
+        return size;
+    }
+
     //offer method to add nodes in Queue
     public void offer(int data){
-        
+
     }
 }
