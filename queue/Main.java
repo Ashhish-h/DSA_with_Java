f9ccd66c60af32b7241fd1620ca50f2a3025c8af
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-02-21 20:05:08 +0530

    add main method and implement all the methods of Queue

--- queue/Main.java
+++ queue/Main.java
@@ -1,5 +1,12 @@
 package queue;
 
+import queue.queue.Queue;
+
 public class Main {
-    
+    public static void main(String[] arhs){
+        Queue queue = new Queue();
+        queue.offer(56);
+        queue.offer(87);
+        queue.offer(68);
+    }
 }
