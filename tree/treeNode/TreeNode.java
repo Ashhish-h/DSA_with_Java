54542cc7e31949c555ae641f87a7bcbeb2844471
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-03-25 22:18:20 +0530

    create TreeNode class

--- tree/treeNode/TreeNode.java
+++ tree/treeNode/TreeNode.java
@@ -1,5 +1,16 @@
 package tree.treeNode;
 
 public class TreeNode {
-    
+    int data;
+    TreeNode leftNode;
+    TreeNode rightNode;
+
+    //constructor
+    public TreeNode(int data){
+       this.data = data;
+       this.leftNode = null;
+       this.rightNode = null; 
+    }
+
+
 }
