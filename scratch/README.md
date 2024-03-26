7ca6dc6ea5841ce9d4bb0ffa1b980d31e34ab453
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-03-26 13:04:41 +0530

    add reademe file to describe why is this package created

---
+++ scratch/README.md
@@ -0,0 +1,3 @@
+This is a scratch package.
+This package is made for testing new logic and custom logic.
+Made your own logic for things and check here is it will work or not then implement it in main file. 
--- scratch/treeScratch/BinaryTree.java
+++ scratch/treeScratch/BinaryTree.java
@@ -1,5 +1,29 @@
 package scratch.treeScratch;
 
+import java.util.Scanner;
+
 public class BinaryTree {
-    
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node rootNode;
+
+    public BinaryTree(Node rootNode){
+        this.rootNode = rootNode;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void insert(int data){
+        
+    }
 }
