01287bd9d3637542b48d9d9c247639805601a1f2
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-04-07 18:34:47 +0530

    exceute methods of BinaryTree in main method

--- tree/main/Main.java
+++ tree/main/Main.java
@@ -1,7 +1,14 @@
 package tree.main;
 
+import java.util.Scanner;
+
+import tree.binarytree.BinaryTree;
+
 public class Main {
     public static void main(String[] ars){
-        
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.insert(scanner);
+        binaryTree.printTree();
     }
 }
