6d50b7612b959f1df021bb305f92a4d32759f9a2
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-04-08 20:51:12 +0530

    add constructors for BinarySearchTree

--- tree/binarySearchTree/BinarySearchTree.java
+++ tree/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,30 @@
 package tree.binarySearchTree;
 
+import tree.treeNode.TreeNode;
+
 public class BinarySearchTree {
-    
+    TreeNode rootNode;
+
+    public BinarySearchTree(){
+        this.rootNode = null;
+    }
+
+    public BinarySearchTree(TreeNode rootNode){
+        this.rootNode = rootNode;
+    }
+
+    public BinarySearchTree(int data, BinarySearchTree leftTree, BinarySearchTree rightTree){
+        this.rootNode = new TreeNode(data);
+        if(leftTree != null){
+            this.rootNode.leftNode = leftTree.rootNode;
+        } else {
+            this.rootNode.leftNode = null;
+        }
+
+        if(rightTree != null){
+            this.rootNode.rightNode = rightTree.rootNode; 
+        } else {
+            this.rootNode.rightNode = null;
+        }
+    }
 }
