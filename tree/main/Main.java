b991f3a57bad7bfe667ee3306af108527caa9a87
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-04-16 20:43:30 +0530

    implement methods of BinarySearchTree class

--- tree/main/Main.java
+++ tree/main/Main.java
@@ -2,13 +2,20 @@ package tree.main;
 
 import java.util.Scanner;
 
+import tree.binarySearchTree.BinarySearchTree;
 import tree.binarytree.BinaryTree;
+import tree.treeNode.TreeNode;
 
 public class Main {
     public static void main(String[] ars){
         Scanner scanner = new Scanner(System.in);
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.insert(scanner);
-        binaryTree.printTree();
+        // BinaryTree binaryTree = new BinaryTree();
+        // binaryTree.insert(scanner);
+        // binaryTree.printTree();
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        TreeNode root = null;
+        
+
     }
 }
