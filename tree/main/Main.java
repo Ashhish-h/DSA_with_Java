3ba5bdfb587e7fcb3027971b60594101f852c43e
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-24 21:03:03 +0530

    implement insert method of BinarySearchTree

--- tree/main/Main.java
+++ tree/main/Main.java
@@ -2,13 +2,19 @@ package tree.main;
 
 import java.util.Scanner;
 
+import tree.binarySearchTree.BinarySearchTree;
 import tree.binarytree.BinaryTree;
 
 public class Main {
     public static void main(String[] ars){
         Scanner scanner = new Scanner(System.in);
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.insert(scanner);
-        binaryTree.printTree();
+        // BinaryTree binaryTree = new BinaryTree();
+        // binaryTree.insert(scanner);
+        // binaryTree.printTree();
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(89);
+        binarySearchTree.insert(35);
+        binarySearchTree.insert(100);
     }
 }
