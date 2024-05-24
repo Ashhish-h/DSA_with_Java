a01b52db4b1584c8d119be3628f7732df1652913
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-24 21:30:17 +0530

    implement non-recursive insert method

--- tree/main/Main.java
+++ tree/main/Main.java
@@ -14,16 +14,24 @@ public class Main {
         // binaryTree.printTree();
 
         BinarySearchTree binarySearchTree = new BinarySearchTree();
-        TreeNode root = null;
+        // TreeNode root = null;
+        // int[] arr = {5, 1, 3, 7, 4, 2};
+        // for(int i = 0; i < arr.length; i++){
+        //     root = binarySearchTree.insert(root, arr[i]);
+        // }
+
+        // binarySearchTree.inOrderTraversal(root);
+        // System.out.println();
+        // TreeNode searchedNode = binarySearchTree.search(7);
+        // System.out.println(searchedNode.data);
+
         int[] arr = {5, 1, 3, 7, 4, 2};
         for(int i = 0; i < arr.length; i++){
-            root = binarySearchTree.insert(root, arr[i]);
+            binarySearchTree.insert(arr[i]);
         }
 
-        binarySearchTree.inOrderTraversal(root);
+        binarySearchTree.inOrderTraversal(binarySearchTree.rootNode);
         System.out.println();
-        TreeNode searchedNode = binarySearchTree.search(7);
-        System.out.println(searchedNode.data);
 
     }
 }
