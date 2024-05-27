e3052c92bf881f29631f5e5ef49b281c3f2cbccb
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-27 13:02:47 +0530

    add insert() method to insert nodes in AVLTree

--- tree/avlTree/AVLTree.java
+++ tree/avlTree/AVLTree.java
@@ -22,4 +22,57 @@ public class AVLTree {
 
         return root.height;
     }
+
+    public int getBalanceFactor(Node root){
+        if(root == null){
+            return 0;
+        }
+        return height(root.leftNode) - height(root.rightNode);
+    }
+
+    public Node insert(Node root, int data){
+        if(root == null){
+            return new Node(data);
+        }
+
+        if(data < root.data){
+            root.leftNode = insert(root.leftNode, data);
+        } else if(data > root.data){
+            root.rightNode = insert(root.rightNode, data);
+        } else {
+            return root;
+        }
+
+        // Update height of the root
+        root.height = 1 + Math.max(height(root.leftNode), height(root.rightNode));
+        
+        // Get root's balance factor
+        int balanceFactor = getBalanceFactor(root);
+
+        // Cases for Balancing :-
+        // Left-Left case
+        if(balanceFactor > 1 && data < root.leftNode.data){
+            return rightRotate(root);
+        }
+
+        //Right-Right case
+        if(balanceFactor < -1 && data > root.rightNode.data){
+            return leftRotate(root);
+        }
+
+        // Left-Right case
+        if(balanceFactor > 1 && data > root.leftNode.data){
+            root.leftNode = leftRotate(root.leftNode);
+            return rightRotate(root);
+        }
+
+        // Right-left case
+        if(balanceFactor < -1 && data < root.rightNode.data){
+            root.rightNode = rightRotate(root.rightNode);
+            return leftRotate(root);
+        }
+
+        return root;
+
+    }
 }
