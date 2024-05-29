e474456f3ca108cfb5bbfb2e1c288c0bdc2806b1
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-29 14:47:59 +0530

    exceute main method

--- divideAndConquer/quicksort/Main.java
+++ divideAndConquer/quicksort/Main.java
@@ -2,6 +2,8 @@ package divideAndConquer.quicksort;
 
 public class Main {
     public static void main(String[] args){
-       
+       int[] arr = {6, 3, 9, 8, 2, 5};
+       QuickSort.quickSort(arr, 0, arr.length - 1);
+       QuickSort.print(arr);
     }
 }
