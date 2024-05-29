34453b08bb8b46b499528c5c3eb224ab4a031df4
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-29 14:28:11 +0530

    add quickSort method to perform sorting on the array

--- divideAndConquer/quicksort/QuickSort.java
+++ divideAndConquer/quicksort/QuickSort.java
@@ -1,5 +1,13 @@
 package divideAndConquer.quicksort;
 
 public class QuickSort {
-    
+    public static void quickSort(int[] arr, int startIndex, int endIndex){
+        if(startIndex >= endIndex){
+            return;
+        }
+
+        int pivotIndex = partition(arr, startIndex, endIndex);
+        quickSort(arr, startIndex, pivotIndex - 1); // For sorting of elements on the left side of the pivot
+        quickSort(arr, pivotIndex + 1, endIndex); // For sorting of elements on the right side of the pivot
+    }
 }
