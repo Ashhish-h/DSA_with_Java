24c8125d4d3c67b0af6eb10c753fbb355f70495e
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-29 14:35:54 +0530

    add partition method that will move the elements around the pivot and return pivot index

--- divideAndConquer/quicksort/QuickSort.java
+++ divideAndConquer/quicksort/QuickSort.java
@@ -10,4 +10,24 @@ public class QuickSort {
         quickSort(arr, startIndex, pivotIndex - 1); // For sorting of elements on the left side of the pivot
         quickSort(arr, pivotIndex + 1, endIndex); // For sorting of elements on the right side of the pivot
     }
+
+    public static int partition(int[] arr, int startIndex, int endIndex){
+        int i = -1; // It will be used to swap elements that are smaller than the pivot with larger elements
+        int pivot = arr[endIndex];
+
+        for(int j = 0; j < endIndex; j++){
+            if(arr[j] <= pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[endIndex] = arr[i];
+        arr[i] = temp;
+
+        return i; // It will return the index  of the pivot
+    }
 }
