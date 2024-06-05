5438a7fbd3e784b8e9747741fd157b2e491726d2
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-06-05 16:34:14 +0530

    add isSafe method to check whether a queen is attacking another queenor not

--- backtracking/NQueens.java
+++ backtracking/NQueens.java
@@ -4,6 +4,32 @@ package backtracking;
 // in such a way that no queen attact each other
 
 public class NQueens {
+    // isSafe method for checking if queens are attacking each other or not
+    public static boolean isSafe(char[][] board, int row, int col){
+        //checking vertically
+        for(int i = row - 1; i >= 0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        // checking left diagonal
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        // checking right diagonal
+        for(int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
     // for simpling placing queens in rows
     public static void nQueens(char[][] board, int row){
         if(row == board.length){
@@ -13,13 +39,17 @@ public class NQueens {
 
         // initilizing board with queens
         for(int index = 0; index < board.length; index++){
-            board[row][index] = 'Q';
-            nQueens(board, row + 1);
-            board[row][index] = 'x';
+            if(isSafe(board, row, index)){
+                board[row][index] = 'Q';
+                nQueens(board, row + 1);
+                board[row][index] = 'x';
+            }
+            
         }
     }
 
     public static void print(char[][] arr){
+        System.out.println("----------Chess Board-----------");
         for(int rowIndex = 0; rowIndex < arr.length; rowIndex++){
             for(int colIndex = 0; colIndex < arr.length; colIndex++){
                 System.out.print(arr[rowIndex][colIndex] + " ");
@@ -29,7 +59,7 @@ public class NQueens {
     }
 
     public static void main(String[] arhs){
-        char[][] board = new char[3][3];
+        char[][] board = new char[4][4];
         for(int rowIndex = 0; rowIndex < board.length; rowIndex++){
             for(int colIndex = 0; colIndex < board.length; colIndex++){
                 board[rowIndex][colIndex] = 'x';
