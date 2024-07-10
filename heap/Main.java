f177770c96c4e5ddb66db6d4bf071aea05d0b50c
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-07-10 14:37:42 +0530

    use method of heap

--- heap/Main.java
+++ heap/Main.java
@@ -1,5 +1,20 @@
-package heap; 
+package heap;
+
+import java.util.ArrayList;
 
 public class Main {
-    
+    public static void main(String[] args) throws Exception{ 
+        Heap<Integer> heap = new Heap<>();
+
+        heap.insert(34);
+        heap.insert(22);
+        heap.insert(44);
+        heap.insert(45);
+        heap.insert(40);
+        
+        // System.out.println(heap.remove());
+
+        ArrayList<Integer> list = heap.heapSort();
+        System.out.println(list);
+    }
 }
