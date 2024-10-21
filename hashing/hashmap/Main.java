d0b99da146eb0299917ef7a4cf416ab1f5431624
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-10-22 00:53:21 +0530

    create main class and add main method to use put method of HashMap.

---
+++ hashing/hashmap/Main.java
@@ -0,0 +1,12 @@
+package hashing.hashmap;
+
+public class Main {
+    public static void main(String[] args){
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Ashish", 9);
+        map.put("Abhishek", 8);
+        map.put("aryan", 7);
+
+
+    }
+}
