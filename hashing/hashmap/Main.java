eacbe45e52d0094b72a09668c0e64fa147ec18bf
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-10-22 19:54:29 +0530

    run containsKey, if it is working or not.

--- hashing/hashmap/Main.java
+++ hashing/hashmap/Main.java
@@ -7,6 +7,7 @@ public class Main {
         map.put("Abhishek", 8);
         map.put("aryan", 7);
 
-
+        boolean isFound = map.containsKey("charu");
+        System.out.println(isFound);
     }
 }
